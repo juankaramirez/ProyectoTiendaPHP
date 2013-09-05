0ff3f2ff26bb2bd4c8446ed04082fe541ea66b60
Juan Camilo Ramirez Padilla <juanram009alt@gmail.com>
2013-09-05 04:53:40 -0500

    Creación y actualización de formularios dinámicos
    JS:
    - Entrada, Edición y Eliminación de categorías.
    - Entrada, Edición y Eliminación de productos.

--- js/custom.js
+++ js/custom.js
@@ -2,23 +2,66 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-$("#catEntrada").click(
-        function() {
-            $("#catEntrada").parent().addClass("active");
+$('#catEntrada').click(
+        function(){
+            
             var html = "";
             html += "<div class='container'><div class='row'><div class='col-md-10'>";
             html += "<form role='form' action='categorias.php' method='POST'>";
             html += "<div class='form-group'><label for='nombreCat'>Nombre</label>";
-            html += "<input name='categoria' type='text' class='form-control' id='nombreProd' placeholder='Nombre'></div>";
+            html += "<input name='nomCat' type='text' class='form-control' id='nombreCat' placeholder='Nombre'></div>";
             html += "<input type='submit' class='btn btn-primary' name='enviar' value='Enviar datos'></form></div></div></div>";
-            $("#catEnt").html(html);
+            
+            if(!$('#catEntrada').parent().hasClass('active')){
+                $("#catFunc").hide().addClass('col-md-4').html(html).show(200);
+                $('#catEntrada').parent().addClass('active');
+                $('#catEditar').parent().removeClass('active');
+                $('#catEliminar').parent().removeClass('active');
+            }
+});
+
+$('#catEditar').click(
+        function (){
+
+            var html = "";
+            html += "<div class='container'><div class='row'><div class='col-md-10'>";
+            html += "<form role='form' action='categorias.php' method='POST'>";
+            html += "<div class='form-group'><div class='row'><div class='col-md-3'><label for='idCat'>Id</label></div>";
+            html += "<div class='col-md-9'><label for='nomCat'>Nombre</label></div></div>";
+            html += "<div class='row'><div class='col-md-3'><input name='idCat' type='text' class='form-control' id='idCat' placeholder='Id'></div>";
+            html += "<div class='col-md-9'><input name='nomCat' type='text' class='form-control' id='nomCat' placeholder='Nombre'></div></div></div>";
+            html += "<input type='submit' class='btn btn-primary' name='editar' value='Editar'></form></div></div></div>";
+            
+            if(!$('#catEditar').parent().hasClass('active')){
+                $("#catFunc").hide().addClass('col-md-4').html(html).show(200);
+                $('#catEntrada').parent().removeClass('active');
+                $('#catEditar').parent().addClass('active');
+                $('#catEliminar').parent().removeClass('active');
+            }           
+ });
+
+    
+$('#catEliminar').click(
+        function() {
+            var html = "";
+            html += "<div class='container'><div class='row'><div class='col-md-10'>";
+            html += "<form role='form' action='categorias.php' method='POST'>";
+            html += "<div class='form-group'><label for='idCat'>Id Categor&iacute;a</label>";
+            html += "<input name='idCat' type='text' class='form-control' id='idCat' placeholder='Id Categor&iacute;a'></div>";
+            html += "<input type='submit' class='btn btn-primary' name='eliminar' value='Eliminar'></form></div></div></div>";
+
+            if (!$('#catEliminar').parent().hasClass('active')) {
+                $("#catFunc").hide().addClass('col-md-4').html(html).show(200);
+                $('#catEntrada').parent().removeClass('active');
+                $('#catEditar').parent().removeClass('active');
+                $('#catEliminar').parent().addClass('active');
+            }
         });
 
-$("#prodEntrada").click(
+
+$('#prodEntrada').click(
         function() {
-            $("#prodEntrada").parent().addClass("active");
+            
             var html = "";
             html += "<div class='container'><div class='row'><div class='col-md-10'>";
             html += "<form role='form' action='productos.php' method='POST'><div class='form-group'>";
@@ -32,6 +75,56 @@ $("#prodEntrada").click(
             html += "<div class='col-md-6'><div class='form-group'><label for='existenciasProd'>Existencias</label>";
             html += "<input name='existencias' type='text' class='form-control' id='existenciaProd' placeholder='Existencias'></div></div></div>";
             html += "<input name='enviar' type='submit' class='btn btn-primary' value='Enviar datos'></form></div></div></div>";
-            html += ""
-            $("#prodEnt").html(html);
-        }); 
+            
+            if(!$('#prodEntrada').parent().hasClass('active')){
+                $("#prodFunc").hide().addClass('col-md-4').html(html).show(200);
+                $('#prodEntrada').parent().addClass('active');
+                $('#prodEditar').parent().removeClass('active');
+                $('#prodEliminar').parent().removeClass('active');
+            }
+            
+}); 
+        
+$('#prodEditar').click(
+        function (){
+
+            var html = "";
+            html += "<div class='container'><div class='row'><div class='col-md-10'>";
+            html += "<form role='form' action='productos.php' method='POST'><div class='row'><div class='col-md-3'><div class='form-group'>";
+            html += "<label for='idProd'>Id</label><input name='idProd' type='text' class='form-control' id='idProd' placeholder='Id'></div></div>";
+            html += "<div class='col-md-9'><div class='form-group'><label for='nombreProd'>Nombre</label><input name='nombre' type='text' class='form-control' id='nombreProd' placeholder='Nombre'></div></div></div>";
+            html += "<div class='row'><div class='col-md-6'><div class='form-group'><label for='idCat'>Id Categor&iacute;a</label>";
+            html += "<input name='idcategoria' type='text' class='form-control' id='nombreCat' placeholder='id Categor&iacute;a'></div></div>";
+            html += "<div class='col-md-6'><div class='form-group'><label for='codigoProd'>C&oacute;digo</label>";
+            html += "<input name='codigo' type='text' class='form-control' id='codigoProd' placeholder='C&oacute;digo'></div></div></div>";
+            html += "<div class='row'><div class='col-md-6'><div class='form-group'><label for='precioProd'>Precio</label>";
+            html += "<input name='precio' type='text' class='form-control' id='precioProd' placeholder='Precio'></div></div>";
+            html += "<div class='col-md-6'><div class='form-group'><label for='existenciasProd'>Existencias</label>";
+            html += "<input name='existencias' type='text' class='form-control' id='existenciaProd' placeholder='Existencias'></div></div></div>";
+            html += "<input name='editar' type='submit' class='btn btn-primary' value='Editar'></form></div></div></div>";
+            
+            if(!$('#prodEditar').parent().hasClass('active')){
+                $("#prodFunc").hide().addClass('col-md-4').html(html).show(200);
+                $('#prodEntrada').parent().removeClass('active'); 
+                $('#prodEliminar').parent().removeClass('active'); 
+                $('#prodEditar').parent().addClass('active');
+            }            
+ });
+ 
+$('#prodEliminar').click(
+        function (){
+
+            var html = "";
+            html += "<div class='container'><div class='row'><div class='col-md-10'>";
+            html += "<form role='form' action='productos.php' method='POST'>";
+            html += "<div class='form-group'><label for='idProd'>Id Producto</label>";
+            html += "<input name='idProd' type='text' class='form-control' id='idProd' placeholder='Id Producto'></div>";
+            html += "<input type='submit' class='btn btn-primary' name='eliminar' value='Eliminar'></form></div></div></div>";
+            
+            if(!$('#prodEliminar').parent().hasClass('active')){
+                $("#prodFunc").hide().addClass('col-md-4').html(html).show(200);
+                $('#prodEntrada').parent().removeClass('active'); 
+                $('#prodEliminar').parent().addClass('active'); 
+                $('#prodEditar').parent().removeClass('active');
+            }
+ });
