a6dc117be175d8c0f6289e5de842054ef53d3ddc
Juan Camilo Ramirez Padilla <juanram009alt@gmail.com>
2013-09-05 04:54:49 -0500

    Código de Edición y Eliminación de la BD.

--- admin/categorias.php
+++ admin/categorias.php
@@ -1,24 +1,55 @@
 <?php
+
 include_once "lib.php";
 
 if (isset($_POST["enviar"])) {
-        //Para no guardar nombres vacios en la BD
-    
-       if (!$_POST["categoria"]=="" && !$db->existeCategoria($_POST["categoria"])) {
+
+       if (!$_POST["nomCat"]=="" && !$db->existeCategoria($_POST["nomCat"])) {
             $cat_temp = new categoria(0, "");
-            $cat_temp->nombre = $_POST["categoria"];
+            $cat_temp->nombre = $_POST["nomCat"];
             $db->adicionarCategoria($cat_temp);
+       } elseif ($_POST["nomCat"]==""){
+           echo '<script type="text/javascript"> alert("El campo está en blanco");</script>';
+       } else {
+           echo '<script type="text/javascript"> alert("La categoría ingresada ya existe en la base de datos");</script>';
+       }
+}
+
+if (isset($_POST["editar"])) {
+    //Para no guardar nombres vacios en la BD
+
+    if (!$_POST["idCat"] == "" && !$_POST["nomCat"]=="" && $db->existeCategoria($_POST["idCat"])) {
+        $cat_temp = $_POST["idCat"];
+        $cat_nom_temp = $_POST["nomCat"];
+        $db->editarCategoria($cat_temp, $cat_nom_temp);
+    } else if ($_POST["idCat"] == "" || $_POST["nomCat"]=="" ) {
+        echo '<script type="text/javascript"> alert("Hay algun(os) campo(s) en blanco");</script>';
+    } else {
+        echo '<script type="text/javascript"> alert("La categoría ingresada no está en la base de datos");</script>';
+        
+    }
+}
+
+if (isset($_POST["eliminar"])) {
+
+       if (!$_POST["idCat"]=="" && $db->existeCategoria($_POST["idCat"])) {
+            $cat_temp = $_POST["idCat"];
+            $db->eliminarCategoria($cat_temp);
+       } elseif ($_POST["idCat"]==""){
+           echo '<script type="text/javascript"> alert("El campo está en blanco");</script>';
+       } else {
+           echo '<script type="text/javascript"> alert("La categoría ingresada no está en la base de datos");</script>';
        }
 }
 ?>
-<!DOCTYPE htmls>
+<!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="../css/custom.css" rel="stylesheet" media="screen">
-        <title>Info</title>
+        <title>Backend</title>
     </head>
     <body>
         <div class="container">
@@ -26,7 +57,6 @@ if (isset($_POST["enviar"])) {
                 <div class="panel-body">
                     <div class="jumbotron">
                         <h1>Registro</h1>
-                        <!--<p class="help-block">Enter your info and log in.</p>-->
                     </div>
 
                     <div class="container" id="content">
@@ -45,28 +75,9 @@ if (isset($_POST["enviar"])) {
                                     <li><a id="catEliminar" href="#">Eliminar</a></li>
                                 </ul>
                             </div>
-                            <div class="col-md-4" id="catEnt">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>Nombre</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        foreach ($db->obtenerTodoCategoria() as $value) {
-                                            echo "<tr>
-                                            <td>{$value->nombre}</td>
-                                            <td><a href='#'>Eliminar</a></td>
-                                            </tr>";
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
+                            <div id="catFunc"></div>
                             <div class="col-md-5">
-                                <h3>Categorias existentes</h3>
+                                <h3>Categor&iacute;as existentes</h3>
                                 <table class="table">
                                     <thead>
                                         <tr>
